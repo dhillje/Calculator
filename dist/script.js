82a1a22c580c0b911aafc7fa87c3325200bfde5a
cawl1010 <dieterhillje@gmail.com>
2022-05-31 14:09:46 -0400

    Added script for values to display

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let input = document.querySelector("#display");
+let btns = document.querySelectorAll("button.number-btn");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const btnValue = btns[i].value;
+    input.innerText = `${btnValue}`;
+  });
+}
