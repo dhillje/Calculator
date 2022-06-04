d8365962b9afcb0da3d63508dbae1b4a988c4701
cawl1010 <dieterhillje@gmail.com>
2022-06-04 15:49:17 -0400

    Change made to display

--- dist/script.js
+++ dist/script.js
@@ -2,13 +2,17 @@
 
 let input = document.querySelector("#display");
 let btns = document.querySelectorAll("button.number-btn");
+let plusBtn = document.querySelector("button.plus-btn");
+let equalBtn = document.querySelector("button.equal-btn");
+
+let firstNum = document.querySelector("#primary");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function (e) {
     e.preventDefault();
 
     const btnValue = parseInt(btns[i].value);
-    const total = btnValue + btnValue;
-    input.innerText = `${total}`;
+    const total = btnValue;
+    firstNum.innerText = `${total}`;
   });
 }
